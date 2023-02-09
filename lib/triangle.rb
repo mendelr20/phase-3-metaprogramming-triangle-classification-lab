5265d72f6316c61be493187a0573230217ab091a
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-02-09 16:33:35 -0500

    commit tree c104a50b7293235103b62bb75689ef2c1817acc4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+
 class Triangle
   # write code here
+  attr_accessor :s1, :s2, :s3
+  def initialize(s1, s2, s3)
+    self.s1 = s1
+    self.s2 = s2
+    self.s3 = s3
+  end
+  def kind
+    if valid_sides?
+      if s1==s2 && s2==s3
+        :equilateral
+      elsif s1==s2 || s1==s3 || s2==s3
+        :isosceles
+      else s1!=s2 && s1!=s3 && s1!=s3
+        :scalene
+      end
+    end
+  end  
+    def valid_sides?
+      if s1<=0 || s2<=0 || s3<=0 || s1+s2<=s3 || s1+s3<=s2 || s2+s3<=s1
+          raise TriangleError 
+      else
+        true  
+      end
+    end
+      class TriangleError < StandardError
+      end
+    
 end
